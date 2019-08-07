9a8c676eadcc9292f73432edcc66ac5d3703c152
AzureAD\alsaddigibrahim <alsaddig_ibrahim@stu.indianhills.edu>
2019-08-07 15:00:58 -0700

    Added Exercise 1.3 File

--- week1/Ibrahim-Exercise1.3.js
+++ week1/Ibrahim-Exercise1.3.js
@@ -1,3 +1,19 @@
+/*
+;=============================================================
+; Title:  Exercise 2.3
+; Author: Alsaddig Ibrahim
+; Date:   9 June 2019
+; Description: Displays a formatted FirstName LastName
+===============================================================
+
+*/
+//header
+const header = require('../Ibrahim-header.js');
+
+console.log(header.display("Alsaddig", "Ibrahim", "Week1 Exercise 1.3"));
+   
+//start code
+console.log("");
 var url = require("url");
 
 var parsedURL = url.parse("https://www.example.com/profile?name=smith");
@@ -7,4 +23,5 @@ console.log(parsedURL.protocol);
 console.log(parsedURL.host);
 
 console.log(parsedURL.query);
+//end code
 
