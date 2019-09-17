229d16bcbe693c76795eaf47beaa6623bab26d35
AzureAD\alsaddigibrahim <alsaddig_ibrahim@stu.indianhills.edu>
2019-09-17 08:18:54 -0700

    Added ems file

--- ems/app.js
+++ ems/app.js
@@ -23,6 +23,46 @@ app.get("/", function (request, response) {
     });
 
 });
+function list(request, response) {  
+
+    var customers = pjs.query("SELECT customerName FROM customers");
+    
+    response.render("list.ejs", { customers });
+  
+  }
+/* app.get('/li',(req, res) => {
+    res.render("employee/addOrEdit");
+      });
+    
+      app.post('/',(req, res) => {
+    insertRecord(req, res);
+      });
+function insertRecord(req, res) {
+    var employee = new Employee();
+    employee.fullName = req.body.fullname;
+    employee.email = req.body.email;
+    employee.mobile = req.body.mobile;
+    employee.city = req.body.city;
+    employee.save((err, doc) => {
+  if (!err)
+    res.redirect('employee/list');
+  else {
+      console.log('Error during record insertions : ' + err);
+        }
+    });
+}
+
+app.get('/list', (req, res) => {
+    Employee.find((err, docs) => {
+  if (!err) {
+      res.render("employee/list", {
+          list: docs
+      });
+  } else {
+      console.log('Error in retrieving employee list :' + err);
+  }
+});
+}); */
 
 http.createServer(app).listen(3000, function() {
 
